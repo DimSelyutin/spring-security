d52fe3e2ba5f37720b55223292a527d05157778c
DimSelyutin <dragnilll78@gmail.com>
2024-07-05 15:48:27 +0300

    test: add JwtProviderTest

--- src/test/java/by/plamya/project/security/JWTTokenProviderTest.java
+++ src/test/java/by/plamya/project/security/JWTTokenProviderTest.java
@@ -1,11 +1,16 @@
 package by.plamya.project.security;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.Optional;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
 import org.springframework.security.core.Authentication;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import by.plamya.project.entity.User;
+import by.plamya.project.utils.constants.SecurityConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -13,50 +18,78 @@ import org.mockito.Mockito;
 public class JWTTokenProviderTest {
 
     // Тест генерации JWT токена
-    // @Test
-    // public void generateTokenTest() {
-    //     // Подготовка данных для теста
-    //     User user = new User(1L, "test@test.com", "testuser", "password", null);
-    //     Authentication authentication = Mockito.mock(Authentication.class);
-    //     Mockito.when(authentication.getPrincipal()).thenReturn(user);
+    // @Disabled
+    @Test
+    public void generateTokenTest() {
+        // Подготовка данных для теста
+        User user = new User(1L, "test@test.com", "testuser", "password", null);
+        Authentication authentication = Mockito.mock(Authentication.class);
+        Mockito.when(authentication.getPrincipal()).thenReturn(user);
 
-    //     // Создание объекта JWTTokenProvider
-    //     JWTTokenProvider tokenProvider = new JWTTokenProvider();
+        // Создание объекта JWTTokenProvider
+        JWTTokenProvider tokenProvider = new JWTTokenProvider();
 
-    //     // Вызов метода generateToken
-    //     String token = tokenProvider.generateToken(authentication);
-    //     // Проверка результата
-    //     assertNotNull(token); // Проверка, что токен сгенерирован успешно
-    // }
+        // Вызов метода generateToken
+        String token = tokenProvider.generateToken(user);
+        // Проверка результата
+        assertNotNull(token); // Проверка, что токен сгенерирован успешно
+    }
 
-    // // Тест валидации токена
-    // @Test
-    // public void validateTokenTest() {
-    //     // Подготовка данных для теста
-    //     User user = new User(1L, "test@test.com", "testuser", "password", null);
-    //     Authentication authentication = Mockito.mock(Authentication.class);
-    //     Mockito.when(authentication.getPrincipal()).thenReturn(user);
-    //     // Создание объекта JWTTokenProvider
-    //     JWTTokenProvider tokenProvider = new JWTTokenProvider();
+    // Тест валидации токена
+    // @Disabled
+    @Test
+    public void validateTokenTest() {
+        // Подготовка данных для теста
+        User user = new User(1L, "test@test.com", "testuser", "password", null);
+        Authentication authentication = Mockito.mock(Authentication.class);
+        Mockito.when(authentication.getPrincipal()).thenReturn(user);
+        // Создание объекта JWTTokenProvider
+        JWTTokenProvider tokenProvider = new JWTTokenProvider();
 
-    //     String validToken = tokenProvider.generateToken(authentication);
+        String validToken = tokenProvider.generateToken(user);
 
-    //     assertTrue(tokenProvider.validateToken(validToken)); // Проверка на успешную валидацию токена
-    // }
+        assertTrue(tokenProvider.validateToken(validToken)); // Проверка на успешную валидацию токена
+    }
 
-    // // Тест извлечения идентификатора пользователя из токена
-    // @Test
-    // public void getUserIdFromTokenTest() {
-    //     // Подготовка данных для теста
-    //     User user = new User(123456L, "test@test.com", "testuser", "password", null);
-    //     Authentication authentication = Mockito.mock(Authentication.class);
-    //     Mockito.when(authentication.getPrincipal()).thenReturn(user);
-    //     // Создание объекта JWTTokenProvider
-    //     JWTTokenProvider tokenProvider = new JWTTokenProvider();
-    //     String validToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWQiOiIxIiwiZW1haWwiOiJkaW1rYTJAbWFpbC5ydSIsInVzZXJuYW1lIjoidmFudXNoa2EyIiwibGFzdG5hbWUiOm51bGwsImlhdCI6MTcxODU2MDkxNSwiZXhwIjoxNzE4NTYxNTE1fQ.FHSzCcNzsbJILpyIiL8g1YlqrXGK9muQ2BEEeo0TzryNrfk6Co4eL7ISWS7VitEeQTgcK-vlmWY5l8c5aqHeoQ";
+    // Тест извлечения идентификатора пользователя из токена
+    // @Disabled
+    @Test
+    public void getUserIdFromTokenTest() {
+        // Подготовка данных для теста
+        User user = new User(123456L, "test@test.com", "testuser", "password", null);
+        Authentication authentication = Mockito.mock(Authentication.class);
+        Mockito.when(authentication.getPrincipal()).thenReturn(user);
+        // Создание объекта JWTTokenProvider
+        JWTTokenProvider tokenProvider = new JWTTokenProvider();
+        String validToken = tokenProvider.generateToken(user);
+        // String validToken =
+        // "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWQiOiIxIiwiZW1haWwiOiJkaW1rYTJAbWFpbC5ydSIsInVzZXJuYW1lIjoidmFudXNoa2EyIiwibGFzdG5hbWUiOm51bGwsImlhdCI6MTcxODU2MDkxNSwiZXhwIjoxNzE4NTYxNTE1fQ.FHSzCcNzsbJILpyIiL8g1YlqrXGK9muQ2BEEeo0TzryNrfk6Co4eL7ISWS7VitEeQTgcK-vlmWY5l8c5aqHeoQ";
 
-    //     Long userId = tokenProvider.getUserIdFromToken(validToken);
+        Long userId = tokenProvider.getUserIdFromToken(validToken);
 
-    //     assertEquals(123456L, userId); // Проверка успешного извлечения идентификатора
-    // }
+        assertEquals(123456L, userId); // Проверка успешного извлечения идентификатора
+    }
+
+    @Test
+    public void testExpiredToken() {
+        // Подготовка данных для теста
+        JWTTokenProvider tokenProvider = new JWTTokenProvider();
+        String expiredToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWQiOiIxIiwiZW1haWwiOiJkaW1rYTJAbWFpbC5ydSIsInVzZXJuYW1lIjoidmFudXNoa2EyIiwibGFzdG5hbWUiOm51bGwsImlhdCI6MTcxODU2MDkxNSwiZXhwIjoxNzE4NTYxNTE1fQ.FHSzCcNzsbJILpyIiL8g1YlqrXGK9muQ2BEEeo0TzryNrfk6Co4eL7ISWS7VitEeQTgcK-vlmWY5l8c5aqHeoQ";
+
+        boolean isTokenExpired = checkIfTokenExpired(tokenProvider, expiredToken);
+
+        assertTrue(isTokenExpired, "Токен должен быть истекшим");
+    }
+
+    private boolean checkIfTokenExpired(JWTTokenProvider tokenProvider, String token) {
+        try {
+            Claims claims = Jwts.parser().setSigningKey(SecurityConstants.SECRET).parseClaimsJws(token).getBody();
+            return claims.getExpiration().before(new Date());
+        } catch (ExpiredJwtException e) {
+            return true;
+        } catch (Exception e) {
+            System.out.println("Ошибка при проверке токена: " + e.getMessage());
+            return false;
+        }
+    }
 }
