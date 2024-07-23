039851fab0d47c9c0faff5d245bda136dd230059
DimSelyutin <dragnilll78@gmail.com>
2024-07-23 15:38:48 +0300

    fix: clean code

--- src/main/java/by/plamya/project/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/by/plamya/project/service/impl/AuthenticationServiceImpl.java
@@ -32,10 +32,12 @@ import by.plamya.project.service.UserService;
 import by.plamya.project.utils.constants.ResponseConstant;
 import by.plamya.project.utils.constants.SecurityConstants;
 import by.plamya.project.utils.enums.ERole;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
 
     private AuthenticationManager authenticationManager;
@@ -47,48 +49,38 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private EmailService emailService;
     private UserFacade userFacade;
 
-    public AuthenticationServiceImpl(AuthenticationManager authenticationManager, UserRepository userRepository,
-            UserService userService, JWTTokenProvider jwtTokenProvider, BCryptPasswordEncoder bCryptPasswordEncoder,
-            ResetTokenService resetTokenService, EmailService emailService, UserFacade userFacade) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.userService = userService;
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        this.resetTokenService = resetTokenService;
-        this.emailService = emailService;
-        this.userFacade = userFacade;
-    }
-
     @Override
     public Map<String, Object> login(LoginRequest loginRequest) {
-
-        User opUser = userRepository.findByEmail(loginRequest.email())
-                .orElseThrow(() -> new UserNotFoundException("User not found"));
-        if (opUser.getPassword() == null) {
-            sendPasswordResetCode(opUser.getEmail());
-            throw new UserWithoutPasswordException(
-                    "Check your email for set password! Email: " + loginRequest.email());
-        }
-
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                loginRequest.email(), loginRequest.password()));
-        User user = userService.getCurrentUser(authentication);
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        User user = validateAndAuthenticateUser(loginRequest);
         String jwt = SecurityConstants.TOKEN_PREFIX + jwtTokenProvider.generateToken(user);
 
         Map<String, Object> response = new HashMap<>();
         response.put("user", user);
         response.put("token", jwt);
         return response;
+    }
 
+    private User validateAndAuthenticateUser(LoginRequest loginRequest) {
+        User user = userRepository.findByEmail(loginRequest.email())
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        if (user.getPassword() == null) {
+            sendPasswordResetCode(user.getEmail());
+            throw new UserWithoutPasswordException(
+                    "Check your email for set password! Email: " + loginRequest.email());
+        }
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return userService.getCurrentUser(authentication);
     }
 
     @Transactional
     @Override
     public UserDTO registerUser(SignupRequest signupRequest) {
-
         User user = mapSignupRequestToUser(signupRequest);
 
         try {
@@ -103,31 +95,34 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User registerOauth2User(Authentication authentication) {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        log.info("oAuth obj {}", oAuth2User);
+        log.info("OAuth obj {}", oAuth2User);
+
         String email = oAuth2User.getAttribute("email");
-        String firstname = oAuth2User.getAttribute("name");
-        String lastname = oAuth2User.getAttribute("family_name");
-        String photo_link = oAuth2User.getAttribute("picture");
+        String firstName = oAuth2User.getAttribute("name");
+        String lastName = oAuth2User.getAttribute("family_name");
+        String photoLink = oAuth2User.getAttribute("picture");
 
-        return userService.createUser(email, firstname, lastname, photo_link);
-
+        return userService.createUser(email, firstName, lastName, photoLink);
     }
 
     @Override
     public String activateUser(String code) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'activateUser'");
     }
 
     @Override
     public String sendPasswordResetCode(String email) {
-        // генерация токена
         String token = resetTokenService.generateResetToken(email);
-        log.info("Sendin token on email: {}", email);
-        EmailDetails emailDetails = new EmailDetails(email, "Yor token is:" + token,
+
+        log.info("Sending token on email: {}", email);
+
+        EmailDetails emailDetails = new EmailDetails(email, "Your token is: " + token,
                 "Reset password Token", null);
+
         emailService.sendSimpleMail(emailDetails);
+
         log.info("Sending link on email for change password email: {}", email);
+
         return token;
     }
 
@@ -138,36 +133,42 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         if (result != null) {
             return result;
         }
+
         User user = userService.getUserByPasswordResetToken(passwordDTO.getToken());
+
         if (!user.getEmail().equals(passwordDTO.getEmail())) {
             throw new ResetTokenException("Check input data!");
         }
+
         userService.changeUserPassword(user, passwordDTO.getNewPassword());
+
         resetTokenService.deleteToken(passwordDTO.getToken());
+
         return result;
     }
 
     @Override
     @Transactional
     public User registerOauth2User(String provider, OAuth2UserInfo oAuth2UserInfo) {
-        User user = new User();
-        user.setEmail(oAuth2UserInfo.getEmail());
-        user.setFirstname(oAuth2UserInfo.getFirstName());
-        user.setFirstname(oAuth2UserInfo.getLastName());
-        user.setActive(1);
-        user.setRoles(Collections.singleton(ERole.ROLE_USER));
-        // user.setProvider(AuthProvider.valueOf(provider.toUpperCase()));
-        return userRepository.save(user);
+        return createOrUpdateOauth2User(new User(), provider, oAuth2UserInfo);
     }
 
     @Override
     @Transactional
     public User updateOauth2User(User user, String provider, OAuth2UserInfo oAuth2UserInfo) {
-        user.setFirstname(oAuth2UserInfo.getFirstName());
-        user.setFirstname(oAuth2UserInfo.getLastName());
-        user.setEmail(oAuth2UserInfo.getEmail());
+        return createOrUpdateOauth2User(user, provider, oAuth2UserInfo);
+    }
 
-        // user.setProvider(AuthProvider.valueOf(provider.toUpperCase()));
+    private User createOrUpdateOauth2User(User user, String provider, OAuth2UserInfo oAuth2UserInfo) {
+        user.setEmail(oAuth2UserInfo.getEmail());
+        user.setFirstname(oAuth2UserInfo.getFirstName());
+        user.setLastname(oAuth2UserInfo.getLastName());
+
+        // Устанавливаем активность пользователя и роли:
+        user.setActive(1);
+
+        user.setRoles(Collections.singleton(ERole.ROLE_USER));
+
         return userRepository.save(user);
     }
 
